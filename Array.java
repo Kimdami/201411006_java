00a83df89334630c227371c62d4fc96801114bd0
Kimdami <bibi112@naver.com>
2016-11-14 10:43:29 +0900

    1-1,1-2 배열생성하고 배열에 있는 수의 합계 구하기

---
+++ Array.java
@@ -0,0 +1,27 @@
+public class Array{
+   
+    public void List(int[] list){
+        int sum =0;
+        for(int i =0; i<list.length; i++){
+            if (list[i]%4==0 && list[i]%5 !=0){
+                sum=sum+list[i];
+            }
+            
+        }
+   
+    	    System.out.println(sum);
+    }
+    
+    public static void main(String[] args){
+        
+        int[] list = new int[1000];
+        
+        for(int i =0; i<1000; i++){
+            list[i]=i+1;
+            }
+
+        Array a = new Array();
+        a.List(list);
+
+    }
+}
